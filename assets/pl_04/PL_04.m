b2746933bc64a534ba495568bdbf5cc3cf266a65
Leandro Gabriel <lassisg@gmail.com>
2020-05-06 15:23:21 +0100

    Exercise 5 done.

--- assets/pl_04/PL_04.m
+++ assets/pl_04/PL_04.m
@@ -24,40 +24,39 @@ clc;
 % int_v >= 8;
 % int >= (img_mean - 1);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 2.
+% img_2 = imread("imp_digital.tif");
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 2.A.
-% Img_2 = imread("imp_digital.tif");
-% Img_2_double = im2double(Img_2);
+% img_2_double = im2double(img_2);
 % figure()
-% subplot(1, 2, 1), imshow(Img_2_double)
-% subplot(1, 2, 2), imhist(Img_2_double)
+% subplot(1, 2, 1), imshow(img_2_double)
+% subplot(1, 2, 2), imhist(img_2_double)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 2.B.
-% Img_2 = imread("imp_digital.tif");
-% Img_2_double = im2double(Img_2);
+% img_2_double = im2double(img_2);
 % threshold = 0.48235; % from histogram visualization (using zoom tool)
-% Img_2_binary = im2bw(Img_2_double, threshold);
+% img_2_binary = im2bw(img_2_double, threshold);
 % figure()
-% subplot(1, 2, 1), imshow(Img_2_double)
-% subplot(1, 2, 2), imshow(Img_2_binary)
+% subplot(1, 2, 1), imshow(img_2_double)
+% subplot(1, 2, 2), imshow(img_2_binary)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 2.C.
-% Img_2 = imread("imp_digital.tif");
-% Img_2_double = im2double(Img_2);
+% img_2_double = im2double(img_2);
 
 % threshold = 0.48235; % from histogram visualization (using zoom tool)
-% Img_2_binary = im2bw(Img_2_double, threshold);
+% img_2_binary = im2bw(img_2_double, threshold);
 
-% [T, MS] = graythresh(Img_2_double);
-% Img_2_binary_otsu = im2bw(Img_2_double, T);
+% [T, MS] = graythresh(img_2_double);
+% img_2_binary_otsu = im2bw(img_2_double, T);
 % % T = Threshold by Otsu method
 % % MS = measure of separability
 
-% threshold
 % T
 
 % figure()
-% subplot(1, 2, 1), imshow(Img_2_binary)
-% subplot(1, 2, 2), imshow(Img_2_binary_otsu)
+% subplot(1, 2, 1), imshow(img_2_binary)
+% subplot(1, 2, 2), imshow(img_2_binary_otsu)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 3.
 % Img_3 = imread("celulas_1.tif");
@@ -87,8 +86,10 @@ clc;
 % subplot(1, 2, 1), imshow(img_4_double)
 % subplot(1, 2, 2), imshow(img_4_double_2_levels, [])
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 5.A.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 5.
 % img_5 = imread("smarties.tif");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 5.A.
 % [rows, cols] = size(img_5);
 
 % % Defining slices
@@ -137,7 +138,6 @@ clc;
 % subplot(1, 3, 3), imshow(img_5_final, []), title('img\_5\_final')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 5.B.
-% img_5 = imread("smarties.tif");
 % [rows, cols] = size(img_5);
 
 % % Defining slices
@@ -172,39 +172,86 @@ clc;
 % subplot(2, 2, 4), imshow(img_5_binary_otsu, []), title('img\_5\_binary\_otsu')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 5.C.
-img_5 = imread("smarties.tif");
+% % Solution inspired by PL_03, exercise 5.D.
+% img_5_bkg = ordfilt2(img_5, 525, ones(25, 25));
+% img_5_mg = imsubtract(img_5_bkg, img_5);
+% img_5_bin = imbinarize(img_5_mg, 'adaptive');
+% img_5_open = imopen(img_5_bin, strel('disk', 5));
+% img_5_final = imopen(img_5_open, strel('square', 5));
 
-% Solution inspired by PL_03, exercise 5.D.
-img_5_bkg = ordfilt2(img_5, 525, ones(25, 25));
-img_5_mg = imsubtract(img_5_bkg, img_5);
-img_5_bin = imbinarize(img_5_mg, 'adaptive');
-img_5_open = imopen(img_5_bin, strel('disk', 5));
-img_5_final = imopen(img_5_open, strel('square', 5));
-
-figure(),
-subplot(1, 2, 1), imshow(img_5_bkg), title('background'),
-subplot(1, 2, 2), imshow(img_5_final), title('segmented image')
+% figure(),
+% subplot(1, 2, 1), imshow(img_5_bkg), title('background'),
+% subplot(1, 2, 2), imshow(img_5_final), title('segmented image')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 6.
-% In the “Hubble.tif” image (left figure) we aim to segment only the objects
-% with the brightest nucleus to obtain a final image similar to that shown in
-% the center. After verifying that a threshold value equal to 90 (in the range
-% 0-255) allowed the correct detection of the contours of the above mentioned
-% objects, we applied a segmentation method based on histogram with this global
-% threshold value obtaining the image shown on the right. As can be seen, this
-% result is not satisfactory since all image objects are segmented. To solve
-% this problem, we chose to use a segmentation method based on region growing.
+img_6 = imread("Hubble.tif");
+T = 90;
+img_6_gt = imquantize(img_6, T);
+
+% figure(),
+% subplot(1, 2, 1), imshow(img_6), title('original image'),
+% subplot(1, 2, 2), imshow(img_6_gt, []), title('global T'),
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 6.A.
-% Analyze the function to perform region growing segmentation whose code is
-% available in the file “regiongrow.m”.
-% [g, NR, SI, TI] = regiongrow(f, S, T)
+% S = 90;
+% T = 90;
+% [g_1, NR, SI, TI] = regiongrow(img_6, S, T);
+% [g_2, NR, SI, TI] = regiongrow(img_6, 100, T);
+% [g_3, NR, SI, TI] = regiongrow(img_6, 110, T);
+% [g_4, NR, SI, TI] = regiongrow(img_6, 140, T);
+% [g_5, NR, SI, TI] = regiongrow(img_6, 180, T);
+% [g_6, NR, SI, TI] = regiongrow(img_6, 80, T);
+% [g_7, NR, SI, TI] = regiongrow(img_6, 70, T);
+% [g_8, NR, SI, TI] = regiongrow(img_6, 40, T);
+% [g_9, NR, SI, TI] = regiongrow(img_6, 45, T);
+
+% S = 240;
+% T = 240;
+% [g_10, NR, SI, TI] = regiongrow(img_6, S, T);
+% [g_11, NR, SI, TI] = regiongrow(img_6, 250, T);
+% [g_12, NR, SI, TI] = regiongrow(img_6, 260, T);
+% [g_13, NR, SI, TI] = regiongrow(img_6, 290, T);
+% [g_14, NR, SI, TI] = regiongrow(img_6, 480, T);
+% [g_15, NR, SI, TI] = regiongrow(img_6, 230, T);
+% [g_16, NR, SI, TI] = regiongrow(img_6, 220, T);
+% [g_17, NR, SI, TI] = regiongrow(img_6, 190, T);
+% [g_18, NR, SI, TI] = regiongrow(img_6, 120, T);
+
+% figure(),
+% subplot(1, 2, 1), imshow(img_6), title('original image'),
+% subplot(1, 2, 2), imshow(img_6_gt, []), title('global T'),
+
+% figure(),
+% subplot(3, 6, 1), imshow(g_1, []), title('T = 90; S = 90'),
+% subplot(3, 6, 2), imshow(g_2, []), title('T = 90; S = (90+10)'),
+% subplot(3, 6, 3), imshow(g_3, []), title('T = 90; S = (90+20)'),
+% subplot(3, 6, 4), imshow(g_4, []), title('T = 90; S = (90+50)'),
+% subplot(3, 6, 5), imshow(g_5, []), title('T = 90; S = (90*2)'),
+% subplot(3, 6, 6), imshow(g_6, []), title('T = 90; S = (90-10)'),
+% subplot(3, 6, 7), imshow(g_7, []), title('T = 90; S = (90-20)'),
+% subplot(3, 6, 8), imshow(g_8, []), title('T = 90; S = (90-50)'),
+% subplot(3, 6, 9), imshow(g_9, []), title('T = 90; S = (90/2)'),
+% subplot(3, 6, 10), imshow(g_10, []), title('T = 240; S = 240'),
+% subplot(3, 6, 11), imshow(g_11, []), title('T = 240; S = (240+10)'),
+% subplot(3, 6, 12), imshow(g_12, []), title('T = 240; S = (240+20)'),
+% subplot(3, 6, 13), imshow(g_13, []), title('T = 240; S = (240+50)'),
+% subplot(3, 6, 14), imshow(g_14, []), title('T = 240; S = (240*2)'),
+% subplot(3, 6, 15), imshow(g_15, []), title('T = 240; S = (240-10)'),
+% subplot(3, 6, 16), imshow(g_16, []), title('T = 240; S = (240-20)'),
+% subplot(3, 6, 17), imshow(g_17, []), title('T = 240; S = (240-50)'),
+% subplot(3, 6, 18), imshow(g_18, []), title('T = 240; S = (240/2)'),
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 6.B.
-% Write a set of instructions to segment the “Hubble.tif” image and get an
-% image similar to that shown in the center. (To create the seed image for the
-% region growing you may consider that all the objects we want to segment have
-% nuclei with intensity greater than 240).
+S = img_6 > 240;
+[img_6_final, NR, SI, TI] = regiongrow(img_6, S, T);
+
+figure(),
+subplot(1, 3, 1), imshow(img_6), title('original image'),
+subplot(1, 3, 2), imshow(im2bw(img_6_final), []), title('Final image bin'),
+% subplot(1, 3, 2), imshow(img_6_final, []), title('final image'),
+subplot(1, 3, 3), imshow(img_6_gt, []), title('global T'),
+% subplot(2, 3, 5), imshow(TI, []), title('TI'),
+% subplot(2, 3, 6), imshow(SI, []), title('SI'),
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 7.
 % Use the K-means clustering for segmenting the “retinograma.tif” image. Use
